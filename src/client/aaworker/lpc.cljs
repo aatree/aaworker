63591dbf56baa06b496e872c89d25b2352eaf5b4
Bill La Forge <laforge49@gmail.com>
2016-02-07 15:13:43 -0500

    notices are working

--- src/client/aaworker/lpc.cljs
+++ src/client/aaworker/lpc.cljs
@@ -4,23 +4,35 @@
 (def worker-map (atom {}))
 
 (defn process-message [file-name event]
-  (let [[fn-key success result] (read-string(.-data event))
-        [state error loading] (get-in @worker-map [file-name 1 fn-key])]
-    (reset! loading nil)
-    (if success
-      (do
+  (let [[fn-key message-type result] (read-string(.-data event))]
+    (cond
+      (= message-type :success)
+      (let [[state error loading] (get-in @worker-map [file-name 1 fn-key])]
+        (reset! loading nil)
         (reset! error nil)
         (reset! state result))
-      (reset! error result))))
+      (= message-type :failure)
+      (let [[state error loading] (get-in @worker-map [file-name 1 fn-key])]
+        (reset! loading nil)
+        (reset! error result))
+      (= message-type :notice)
+      (let [f (get-in @worker-map [file-name 2 fn-key])]
+        (if f (apply f result))))))
+
+(defn register-responder [file-name fn-name rsp-vec]
+  (swap! worker-map assoc-in [file-name 1 (keyword fn-name)] rsp-vec))
+
+(defn register-notice-processor [file-name key f]
+  (swap! worker-map assoc-in [file-name 2 key] f))
 
 (defn new-worker [file-name]
   (let [w (js/Worker. file-name)]
     (swap! worker-map assoc file-name
-           [w {}])
-    (set! (.-onmessage w) (partial process-message file-name))))
-
-(defn register-responder [file-name fn-name rsp-vec]
-  (swap! worker-map assoc-in [file-name 1 (keyword fn-name)] rsp-vec))
+           [w {} {}])
+    (set! (.-onmessage w) (partial process-message file-name)))
+  (register-notice-processor file-name :alert
+                             (fn [msg]
+                               (js/alert msg))))
 
 (defn mklocal [fn-name file-name state error loading]
   (register-responder file-name fn-name [state error loading])
--- src/worker/aaworker/api.cljs
+++ src/worker/aaworker/api.cljs
@@ -6,6 +6,9 @@
 (defn register-fn [fn-name f]
   (swap! worker-fn-map assoc (keyword fn-name) f))
 
+(defn send-notice [key & args]
+  (.postMessage js/self (prn-str [key :notice args])))
+
 (defn process-request [event]
   (let [data (read-string (.-data event))
         fn-key (first data)
@@ -13,10 +16,10 @@
     (try
       (let [fn (fn-key @worker-fn-map)
             rv (apply fn args)
-            msg (prn-str [fn-key true rv])]
+            msg (prn-str [fn-key :success rv])]
         (.postMessage js/self msg))
       (catch :default e
-        (.postMessage js/self (prn-str [fn-key false e]))))))
+        (.postMessage js/self (prn-str [fn-key :failure e]))))))
 
 (defn process-requests []
   (set! (.-onmessage js/self) process-request))
